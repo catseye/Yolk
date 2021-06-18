9704e896a46bcb38d485bd07690fe7112fdd43d5
Chris Pressey <cpressey@catseye.tc>
2021-06-18 15:46:08 +0100

    Rewrite the Scheme implementation in continuation-passing style.

--- impl/yolk.scm/yolk.scm
+++ impl/yolk.scm/yolk.scm
@@ -27,6 +27,34 @@
         (eval-yolk full (head (tail (tail (tail prog)))) arg)
         (eval-yolk full (head (tail (tail (tail (tail prog))))) arg)))
     ((eq? (head prog) 'self)
-      (eval-yolk full full (eval-yolk full (head (tail prog) arg))))
+      (eval-yolk full full (eval-yolk full (head (tail prog)) arg)))
     (else
       (head 'head)))))
+
+; A rewriting of the above in continuation-passing style.
+
+(define eval-yolk-k (lambda (full prog arg k)
+  (cond
+    ((eq? prog 'arg)
+      (k arg))
+    ((eq? (head prog) 'head)
+      (eval-yolk-k full (head (tail prog)) arg (lambda (r) (k (head r)))))
+    ((eq? (head prog) 'tail)
+      (eval-yolk-k full (head (tail prog)) arg (lambda (r) (k (tail r)))))
+    ((eq? (head prog) 'cons)
+      (eval-yolk-k full (head (tail prog)) arg (lamdba (h)
+        (eval-yolk-k full (head (tail (tail prog))) arg (lambda (t)
+          (k (cons h t)))))))
+    ((eq? (head prog) 'quote)
+      (k (head (tail prog))))
+    ((eq? (head prog) 'ifeq)
+      (eval-yolk-k full (head (tail prog)) arg (lambda (a)
+        (eval-yolk-k full (head (tail (tail prog))) arg (lambda (b)
+          (if (eq? a b)
+            (eval-yolk-k full (head (tail (tail (tail prog)))) arg k)
+            (eval-yolk-k full (head (tail (tail (tail (tail prog))))) arg k)))))))
+    ((eq? (head prog) 'self)
+      (eval-yolk-k full (head (tail prog)) arg (lambda (newarg)
+        (eval-yolk-k full full newarg k))))
+    (else
+      (k (head 'head))))))
